819276ad63b5f6d64ba08e867d6d179a55f4e657
Your Name <your_email@whatever.com>
2020-09-02 22:03:07 +0300

    hw-08

--- Module-08/src/index.js
+++ Module-08/src/index.js
@@ -1,2 +1,55 @@
 import images from "./gallery-items.js";
-console.log(images);
+let imagesLi = "";
+
+const galleryHtml = images.reduce((imagesLi, image) => {
+  return (imagesLi += `
+  <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${image.original}"
+  >
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`);
+}, "");
+
+const gallery = document.querySelector(".gallery");
+const modalWindow = document.querySelector(".lightbox");
+const img = modalWindow.querySelector(".lightbox__image");
+const modalWindowKeyControl = (event) => {
+  console.log(event);
+  if (event.key === "Escape") {
+    closeModalWindow();
+  }
+};
+const closeModalWindow = () => {
+  document.removeEventListener("keydown", modalWindowKeyControl);
+  modalWindow.classList.remove("is-open");
+  img.src = "";
+};
+
+gallery.insertAdjacentHTML("beforeEnd", galleryHtml);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const { target, currentTarget } = event;
+  if (target.classList.contains("gallery__image")) {
+    img.src = target.dataset.source;
+    document.addEventListener("keydown", modalWindowKeyControl);
+    modalWindow.classList.add("is-open");
+  }
+});
+modalWindow.addEventListener("click", () => {
+  const { target, currentTarget } = event;
+  if (
+    target.dataset.action === "close-lightbox" ||
+    target.classList.contains("lightbox__overlay")
+  ) {
+    closeModalWindow();
+  }
+});
